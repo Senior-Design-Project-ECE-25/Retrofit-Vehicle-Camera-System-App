27534e7432a7d8a27c4ae5665e242545fb5b1cb8
Jack McVeigh <jmcveigh55@gmail.com>
2022-01-31 16:25:47 -0500

    Initial Commit

---
+++ Retrofit Vehicle Camera System/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Retrofit Vehicle Camera System
+//
+//  Created by Jack McVeigh on 1/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Retrofit Vehicle Camera System/Retrofit_Vehicle_Camera_SystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Retrofit_Vehicle_Camera_SystemApp.swift
+//  Retrofit Vehicle Camera System
+//
+//  Created by Jack McVeigh on 1/31/22.
+//
+
+import SwiftUI
+
+@main
+struct Retrofit_Vehicle_Camera_SystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
