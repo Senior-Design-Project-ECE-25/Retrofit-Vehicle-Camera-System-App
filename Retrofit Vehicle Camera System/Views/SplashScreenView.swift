2ee0d9dd536b2b6349a2dab43e644e85daeedfcf
Jack McVeigh <jmcveigh55@gmail.com>
2022-02-02 15:05:28 -0500

    Refactoring project structure to MVVM structure. Adding SplashScreen template.

---
+++ Retrofit Vehicle Camera System/Views/SplashScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashScreenView.swift
+//  Retrofit Vehicle Camera System
+//
+//  Created by Jack McVeigh on 2/2/22.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text("Hello from the Splash Screen!")
+    }
+}
+
+struct SplashScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreenView()
+    }
+}
