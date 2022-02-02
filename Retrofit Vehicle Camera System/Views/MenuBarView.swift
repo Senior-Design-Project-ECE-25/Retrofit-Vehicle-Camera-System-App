b4acfece9ee3feb9b25889ebc3860bd518157e33
Jack McVeigh <jmcveigh55@gmail.com>
2022-02-02 15:45:26 -0500

    Adding the menu bar view.

---
+++ Retrofit Vehicle Camera System/Views/MenuBarView.swift
@@ -0,0 +1,20 @@
+//
+//  MenuBarView.swift
+//  Retrofit Vehicle Camera System
+//
+//  Created by Jack McVeigh on 2/2/22.
+//
+
+import SwiftUI
+
+struct MenuBarView: View {
+    var body: some View {
+        Text("Hello from the menu bar!")
+    }
+}
+
+struct MenuBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuBarView()
+    }
+}
